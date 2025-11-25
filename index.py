a02b6c40e88495e93b102087b1b89fc642ff87ab
amine <dearnyclever@gmail.com>
2025-11-25 11:32:34 +0100

    add - variable

--- index.py
+++ index.py
@@ -1,11 +1,8 @@
 print("Hello, World!")
-
 from exploit import title, description, severity, score
-
 print(f"Title: {title}")
 print(f"Description: {description.strip()}")
 print(f"Severity: {severity}")
 print(f"Score: {score}")
-
-amine_variable = "Bonjour Git-Amine !"
+amine_variable = "Bonjour Git !"
 print(amine_variable)
