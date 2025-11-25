ca1dcf07c74d11d80e85c14af725a79381751b2a
chaouki <behazchaouki@gmail.com>
2025-11-25 11:00:46 +0100

    Add the variable

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
 print("Hello, World!")
 
-from exploit import title, description, severity, score
+from exploit import title, description, severity, score, Chaouki
 
 print(f"Title: {title}")
 print(f"Description: {description.strip()}")
 print(f"Severity: {severity}")
 print(f"Score: {score}")
+print(f"Variable: {Chaouki}")
