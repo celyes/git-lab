a51008c372bb898864ee19573cab8bc817908baa
celyes <celyes02@gmail.com>
2025-11-25 09:38:01 +0100

    Fix vulnerability RCE in X

---
+++ index.py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+
+from exploit import title, description, severity, score
+
+print(f"Title: {title}")
+print(f"Description: {description.strip()}")
+print(f"Severity: {severity}")
+print(f"Score: {score}")
